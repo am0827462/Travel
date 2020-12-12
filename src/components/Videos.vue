60eb937eee3a174a7b37242629420b9d81a67b85
Angela McLaughlin <amclaughlin@SEA.CORP.EXPECN.com>
2020-12-12 10:43:21 -0600

    First Commit

---
+++ src/components/Videos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>Videos</h3>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/YP3Kr9UOqb0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/cdJxRpg39pw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Videos',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
