7842203c25d9e33dab6a32326d2592e3113f7122
Angela McLaughlin <amclaughlin@SEA.CORP.EXPECN.com>
2021-03-25 17:35:15 -0500

    3/25/21

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h3>Videos</h3>
+    <h3>Spring Break - Panama City, FL</h3>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/8LGp-sl39T0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h3>Past Videos</h3>
     <iframe width="540" height="315" src="https://www.youtube.com/embed/CiAG0304FfM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe width="540" height="315" src="https://www.youtube.com/embed/J79b1MsYaVg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe width="540" height="315" src="https://www.youtube.com/embed/cdJxRpg39pw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
