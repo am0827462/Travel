9c79c9a5659783de8ee92d5132af58841e27f19c
Angela McLaughlin <am0827462@otc.edu>
2021-07-01 15:43:14 -0500

    Iceland

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <h3>May Trip - Belize</h3>
+    <h3>Iceland!</h3>
+    <iframe width="800" height="500" src="https://www.youtube.com/embed/H9hRBMfa9Gs" id="iceland" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h3>Past Videos</h3>
     <iframe width="540" height="315" src="https://www.youtube.com/embed/H9e6tyMzDOA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe width="540" height="315" src="https://www.youtube.com/embed/h3EBO3xLEOI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <h3>Past Videos</h3>
     <iframe width="540" height="315" src="https://www.youtube.com/embed/8LGp-sl39T0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe width="540" height="315" src="https://www.youtube.com/embed/CiAG0304FfM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe width="540" height="315" src="https://www.youtube.com/embed/J79b1MsYaVg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -22,6 +23,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+div{
+  text-align:center;
+}
 iframe{
   padding:10px;
 }
